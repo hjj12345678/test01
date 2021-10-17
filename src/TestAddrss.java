f42f60a123db2d32c761b474df055649ae5f2ca2
hjj <547784390@qq.com>
2021-10-17 22:01:11 +0800

    第一次发布

---
+++ src/TestAddrss.java
@@ -0,0 +1,20 @@
+public class TestAddrss {
+   public  static int   abc(int n){
+        if  (n<=2){
+            System.err.println(n+"里面");
+            return 1;
+        }
+       System.err.println(n+"外面");
+        return abc(n-1)+abc(n-2);
+    }
+
+    public static void main(String[] args) {
+      /* int  i=1;
+        System.err.println(++i+ ++i + ++i);*/
+      int c;
+      c=2;
+        System.err.println(c);
+        System.err.println(c++);
+        System.err.println(c);
+    }
+}
